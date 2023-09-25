fd9e39fae89c72099be3992aa2351208ecbc88f0
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-26 07:32:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Data Preprocessing')
 
     parser.add_argument('--num_instances', type=int, default=2, help='Number of shifted instances required.')
+    parser.add_argument('--f_num_instances', type=int, default=2, help='Number of shifted instances required.')
+    parser.add_argument('--delay_group', type=str, default='ARI_CO', help='Number of shifted instances required.')
+    parser.add_argument('--f_delay_group', type=str, default='ARI_CO', help='Number of shifted instances required.')
+    parser.add_argument('--avg_group', type=str, default='ARI_PO', help='Number of shifted instances required.')
+    parser.add_argument('--avg_window', type=int, default=3, help='Number of shifted instances required.')
     parser.add_argument('--file_path', type=str, required=True, help='Path to the data file.')
 
     # Boolean flags for conditional execution of functions
@@ -20,6 +25,8 @@ if __name__ == "__main__":
     parser.add_argument('--preprocess', action='store_true', help='Flag to preprocess data.')
     parser.add_argument('--extract_dates', action='store_true', help='Flag to extract date parts.')
     parser.add_argument('--previous_delays', action='store_true', help='Flag to calculate previous delays.')
+    parser.add_argument('--future_delays', action='store_true', help='Flag to calculate future delays.') 
+    parser.add_argument('--moving_avg', action='store_true', help='Flag to calculate moving_avg.')
 
     parser.add_argument('--datetime_col', type=str, default="datetime_column_name", help='Name of the datetime column if extracting dates.')
 
