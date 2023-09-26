038c67023571b973239a44d9a8b99e016dab9e58
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-27 07:20:23 +0900

    Update submission.py

--- submission.py
+++ submission.py
@@ -4,6 +4,6 @@ import joblib
 def get_submission(pred_path, test_path, name):
   test = pd.read_csv('test_path')
   pred = joblib.load(pred_path)
-  result = pd.concat([pd.Series(pred),(test['TEST_ID'])],axis = 1)
+  result = pd.concat([(test['TEST_ID']),pd.Series(pred)],axis = 1)
   result.columns = (['ID','prediction'])
   result.to_csv(f'{name}.csv',index = False)
