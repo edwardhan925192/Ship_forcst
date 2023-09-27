d2930e8cccd29cc777fd256f735a5da06f92d87d
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-27 09:45:04 +0900

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import joblib
 
-def get_submission(pred_path, test_path, name):
-  test = pd.read_csv('test_path')
+def get_submission(pred_path, test_path, name):    
+
+  test = pd.read_csv(test_path)
   pred = joblib.load(pred_path)
-  result = pd.concat([(test['TEST_ID']),pd.Series(pred)],axis = 1)
+
+  #initializing 
+  test['CI_HOUR'] = 0
+  
+  # Create a mask for rows that do not have 'skip' in column B
+  mask = test['DIST'] != 0
+
+  # Use the mask to filter the rows and assign values from values_list
+  test.loc[mask, 'CI_HOUR'] = pred
+  
+  result = test[['ID','CI_HOUR']]
   result.columns = (['ID','prediction'])
   result.to_csv(f'{name}.csv',index = False)
